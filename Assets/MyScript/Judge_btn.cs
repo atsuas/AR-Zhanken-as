31fb65b677a748a33319282c5acefe89e6decb49
as <>
2021-02-03 08:48:14 +0900

    勝敗判定のスクリプト

--- Assets/MyScript/Judge_btn.cs
+++ Assets/MyScript/Judge_btn.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Judge_btn : MonoBehaviour
 {
-    public GameObject p1_1;
-    public GameObject p1_5;
-    public GameObject p1_10;
-    public GameObject p2_1;
-    public GameObject p2_5;
-    public GameObject p2_10;
+    public GameObject p1_gu;
+    public GameObject p1_thoki;
+    public GameObject p1_pa;
+    public GameObject p2_gu;
+    public GameObject p2_thoki;
+    public GameObject p2_pa;
+    public Text text;
 
     // Start is called before the first frame update
     void Start()
@@ -25,84 +27,86 @@ public class Judge_btn : MonoBehaviour
 
     public void OnClick()
     {
-        Renderer ren_p1_1 = p1_1.GetComponent<Renderer>();
-        Renderer ren_p1_5 = p1_5.GetComponent<Renderer>();
-        Renderer ren_p1_10 = p1_10.GetComponent<Renderer>();
-        Renderer ren_p2_1 = p2_1.GetComponent<Renderer>();
-        Renderer ren_p2_5 = p2_5.GetComponent<Renderer>();
-        Renderer ren_p2_10 = p2_10.GetComponent<Renderer>();
+        Renderer ren_p1_gu = p1_gu.GetComponent<Renderer>();
+        Renderer ren_p1_thoki = p1_thoki.GetComponent<Renderer>();
+        Renderer ren_p1_pa = p1_pa.GetComponent<Renderer>();
+        Renderer ren_p2_gu = p2_gu.GetComponent<Renderer>();
+        Renderer ren_p2_thoki = p2_thoki.GetComponent<Renderer>();
+        Renderer ren_p2_pa = p2_pa.GetComponent<Renderer>();
 
         string p1_card = "";
         string p2_card = "";
 
-        if (ren_p1_1.enabled == true)
+        if (ren_p1_gu.enabled == true)
         {
-            p1_card = "1";
+            p1_card = "gu";
         }
-        if (ren_p1_5.enabled == true)
+        if (ren_p1_thoki.enabled == true)
         {
-            p1_card = "5";
+            p1_card = "thoki";
         }
-        if (ren_p1_10.enabled == true)
+        if (ren_p1_pa.enabled == true)
         {
-            p1_card = "10";
+            p1_card = "pa";
         }
 
-        if (ren_p2_1.enabled == true)
+        if (ren_p2_gu.enabled == true)
         {
-            p1_card = "1";
+            p1_card = "gu";
         }
-        if (ren_p2_5.enabled == true)
+        if (ren_p2_thoki.enabled == true)
         {
-            p1_card = "5";
+            p1_card = "thoki";
         }
-        if (ren_p2_10.enabled == true)
+        if (ren_p2_pa.enabled == true)
         {
-            p1_card = "10";
+            p1_card = "pa";
         }
 
         switch (p1_card)
         {
-            case "1":
-                if (p2_card == "1")
+            case "gu":
+                if (p2_card == "gu")
                 {
+                    text.text = "あいこ";
+                }
+                else if (p2_card == "thoki")
+                {
+                    text.text = "p1の勝ち";
+                }
+                else if (p2_card == "pa")
+                {
+                    text.text = "p1の負け";
+                }
+                break;
+            case "thoki":
+                if (p2_card == "thoki")
+                {
+                    text.text = "あいこ";
+                }
+                else if (p2_card == "pa")
+                {
+                    text.text = "p1の勝ち";
 
                 }
-                else if (p2_card == "10")
+                else if (p2_card == "gu")
                 {
-
-                }
-                else if (p2_card == "5")
-                {
+                    text.text = "p1の負け";
 
                 }
                 break;
-            case "5":
-                if (p2_card == "5")
+            case "pa":
+                if (p2_card == "pa")
                 {
-
+                    text.text = "あいこ";
                 }
-                else if (p2_card == "1")
+                else if (p2_card == "gu")
                 {
-
+                    text.text = "p1の勝ち";
                 }
-                else if (p2_card == "10")
+                else if (p2_card == "thoki")
                 {
-
-                }
-                break;
-            case "10":
-                if (p2_card == "10")
-                {
-
-                }
-                else if (p2_card =="5")
-                {
-
-                }
-                else if (p2_card == "1")
-                {
-
+                    text.text = "p1の負け";
                 }
                 break;
         }
